fce3114b29edfd6919f926fd31a4019d40e9d5ef
nathanl22085 <nathanl.22085@obhs.school.nz>
2025-10-07 13:21:30 +1300

    Fixed salad section and added outFile code/datetime

--- sams_sandwich.py
+++ sams_sandwich.py
@@ -1,4 +1,6 @@
 #Sam's sandwich
+import datetime
+
 def bread_selection():
     bread_list=["White","Brown","Italian","Granary","Rye"]
     count=0
@@ -47,26 +49,44 @@ def salad_selection():
         print(count+1, "  ",salad_list[count])
         count+=1
     print("Press enter/return when you have finished chosing your salads ")
-    salads_added = " " #will hold a string of more than one item
+    salads_added = "" #will hold a string of more than one item
     selected_salad = " "#prompts the user to enter in a number to select a salad
 
-    while selected_salad != " ": #if enter is not pressed it will keep prompting you to enter in a number
-        selected_salad = input(f"What number salad do you want?\nYou have selected: {salads_added}")
-        if selected_salad != " ": #if you press enter this if statemnet will not run
+    while selected_salad != "": #if enter is not pressed it will keep prompting you to enter in a number
+        selected_salad = input(f"What number salad do you want?\n You have selected: {salads_added}")
+        if selected_salad != "": #if you press enter this if statemnet will not run
             selected_salad = int(selected_salad)
             #this variable keeps adding on each selected item from salad list
-            salads_added = salads_added + "  " + salad_list[selected_salad]
+            salads_added = salads_added + "  " + salad_list[selected_salad-1]
     return salads_added.strip() #removes empty space at start of the string
+
+def output_textfile(first_name,cellphone_number,sandwich_order):
+    date_time=datetime.datetime.now()
+    outFile=open("Sam's_Sandwich.txt","a")
+    print(f"\n***Order for {first_name} - {cellphone_number}:***")
+    outFile.write(f"\nDate of booking:{date_time}")
+    for item in sandwich_order:
+        print(item)
+        outFile.write(f"\n End of order: {date_time}")
+    print(f"***End of order : {date_time}***")
+    outFile.write("\n")
+    outFile.write("\n")
+    outFile.close()
 
 #main program
 print("Welcome to Sam's Sandwich Shop")
+first_name=str(input("Please enter in your first name "))
+cellphone_number=str(input("Please enter in your cellphone number "))
 bread_choice=bread_selection() #creating a variable that calls up the bread function and returns their choice
 meat_choice=meat_selection() #creating a variable that calls up the meat function and returns their choice
 cheese_choice=cheese_selection() #creating a variable that calls up the cheese function and returns their choice
 dressing_choice=dressing_selection() #creating a variable that calls up the dressing function and returns their choice
 salad_choice=salad_selection() #creating a variable that calls up the dressing function and returns their choice
-print(f"Your selected bread choice: {bread_choice}")
-print(f"Your selected meat choice: {meat_choice}")
-print(f"Your selected cheese choice: {cheese_choice}")
-print(f"Your selected dressing choice: {dressing_choice}")
-print(f"Your selected salad choices: {salad_choice}")
+sandwich_order=[]
+sandwich_order.append(first_name)
+sandwich_order.append(cellphone_number)
+sandwich_order.append(f"Bread choice: {bread_choice}")
+sandwich_order.append(f"Meat choice: {meat_choice}")
+sandwich_order.append(f"Cheese choice: {cheese_choice}")
+sandwich_order.append(f"Salad choice(s): {salad_choice}")
+output_textfile(first_name,cellphone_number,sandwich_order)
